52008604a7cfb2893b5d5dbbe1b4b82f8125d4cb
Fletcher Thompson <fletcher@me3d.com.au>
2019-03-09 22:57:03 +1100

    Can generate an array of heagon center points

--- hexmapper.py
+++ hexmapper.py
@@ -9,11 +9,12 @@ parameterSourceFile = "./parameters.json"
 def main():
     """Main entry point of script."""
     param = loadParameters(parameterSourceFile)
-
+    hexArray = genArray(param["width"], param["height"], param["hexWidth"], param["alternateRows"])
+    hexArray = colourMap()
     pass
 
 def loadParameters(parameterSource):
-    """Pulls in parameters from parameters.json config file and returns a queriable python dictionary."""
+    """Pulls in config file and returns a queriable python dictionary."""
     try:
         with open(parameterSource, 'r') as paramFile:
             paramDict = json.load(paramFile)
@@ -21,15 +22,22 @@ def loadParameters(parameterSource):
         print("Failed to parse parameters file.")
     return paramDict
 
-def genArray(width, height):
+def genArray(width, height, hexWidth, alternateRows):
     """Creates an array of hex origins and colour assignments."""
-    for 
+    outArray = []
+    for row in range(height):
+        rowOffset =0
+        if alternateRows == "in" and height%2 == 0:
+            rowOffset = 1
+        for piece in range(width - rowOffset):
+            outArray.append((piece*hexWidth+((row % 2)*hexWidth/2), row*hexWidth/1.73205, 0, 0, 0))
+    return outArray
+
+def colourMap (angle, scatter)
 
 def generateSVG():
     """outputs a .csv file of the mapped hex tiles"""
     pass
-
-
 
 if __name__ == '__main__':
     sys.exit(main())
